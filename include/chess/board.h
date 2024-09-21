c00a68a8ead87aa6139fd9a38ff4ef1e3d3332d1
Paul Zupan <paul.zupan.13@gmail.com>
2024-09-20 21:51:05 -0700

    Start implementing Fen parsing

--- include/chess/board.h
+++ include/chess/board.h
@@ -1,35 +1,88 @@
 #pragma once
 
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+/**
+ * This represents a piece. A null byte indicates that the current square has no
+ * piece on it. Perhaps it is not a good design decision to allow the piece enum
+ * to represent that case, but I could not be bothered to do it differently.
+ */
 typedef enum {
-  PIECE_BLACK,
-  PIECE_WHITE,
-} piece_color_t;
-
-typedef enum {
-  PIECE_BISHOP,
-  PIECE_KING,
-  PIECE_KNIGHT,
-  PIECE_PAWN,
-  PIECE_QUEEN,
-  PIECE_ROOK,
-} piece_kind_t;
-
-typedef union {
-
+  PIECE_NONE = '\0',
+  PIECE_BLACK_BISHOP = 'b',
+  PIECE_BLACK_KING = 'k',
+  PIECE_BLACK_KNIGHT = 'n',
+  PIECE_BLACK_PAWN = 'p',
+  PIECE_BLACK_QUEEN = 'q',
+  PIECE_BLACK_ROOK = 'r',
+  PIECE_WHITE_BISHOP = 'B',
+  PIECE_WHITE_KING = 'K',
+  PIECE_WHITE_KNIGHT = 'N',
+  PIECE_WHITE_PAWN = 'P',
+  PIECE_WHITE_QUEEN = 'Q',
+  PIECE_WHITE_ROOK = 'R',
 } piece_t;
 
+/**
+ * The side to move. If it is 'b', it is black's turn to move, and if it is 'w',
+ * it is white to play.
+ */
+typedef enum {
+  SIDE_BLACK = 'b',
+  SIDE_WHITE = 'w',
+} side_to_move_t;
+
+/**
+ * A bit field representing castling rights.
+ */
+typedef enum {
+  CASTLE_NONE = 0x00,
+  CASTLE_WHITE_KING = 0x01,
+  CASTLE_WHITE_QUEEN = 0x02,
+  CASTLE_BLACK_KING = 0x04,
+  CASTLE_BLACK_QUEEN = 0x08,
+} castling_rights_t;
+
+/**
+ * A square on the board is simply an index into the array of pieces. Index 0 of
+ * that array represents the a8 square, and subsequent indices walk down the
+ * ranks of the board, wrapping around to the opposite file when the rank's end
+ * is reached, meaning that the h1 square is index 63.
+ */
+typedef size_t square_t;
+
+/**
+ * Encodes all the information about a chess position that a fen string does. We
+ * need to be able to parse fen strings into positions and compile positions to
+ * fen strings, for which this type should be more than suitable.
+ */
 typedef struct {
-  int field;
+  piece_t pieces[64];
+  side_to_move_t side_to_move;
+  castling_rights_t castling_rights;
+  square_t en_passant_target;
+  uint8_t halfmove_clock;
+  uint16_t fullmove_count;
 } board_t;
 
 /**
  * Parses the null-terminated fen string provided and writes the result to the
  * board pointer. It is an error to pass NULL to either parameter.
  */
-void board_from_fen(const char *fen, board_t *board);
+void board_from_fen(const char *fen, board_t *out_board);
 
 /**
  * Prints the board to standard out. It is an error to pass NULL into this
  * function.
  */
 void board_print(const board_t *board);
+
+/**
+ * Creates a fen string out of a board object. It is up to the caller to ensure
+ * that the buffer passed is large enough to fit the fen string. A buffer that
+ * is 128 bytes large is more than enough to contain any fen string. It is an
+ * error to pass NULL as the board.
+ */
+bool board_to_fen(const board_t *board, char out_fen[]);
--- src/board.c
+++ src/board.c
@@ -1,4 +1,85 @@
 #include "chess/board.h"
 
-void board_from_fen(const char *fen, board_t *board) {}
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * Copies a string into a heap allocated buffer.
+ *
+ * TODO: Finish this function documentation
+ */
+static char *copy_string(const char *s);
+
+/**
+ * Parses castling rights from the given string.
+ *
+ * This function will search the given string for the characters 'K', 'Q', 'k',
+ * and 'q', returning castling rights for the white king, white queen, black
+ * king, and black queen respectively. This function ignores any character that
+ * isn't one of those four, or any duplicates.
+ */
+static castling_rights_t parse_castling_rights(const char *rights_string);
+static void parse_pieces(char *pieces_string, piece_t out_pieces[64]);
+
+void board_from_fen(const char *fen, board_t *board) {
+  char *fen_copy = copy_string(fen);
+
+  parse_pieces(strtok(fen_copy, " "), board->pieces);
+  board->side_to_move = *strtok(NULL, " ");
+  board->castling_rights = parse_castling_rights(strtok(NULL, " "));
+
+  free(fen_copy);
+}
+
 void board_print(const board_t *board) {}
+bool board_to_fen(const board_t *board, char out_fen[]) { return false; }
+
+char *copy_string(const char *s) {
+  size_t size = strlen(s) + 1;
+  char *buffer = malloc(size);
+  if (buffer == NULL) {
+    perror("malloc");
+    exit(1);
+  }
+  return memcpy(buffer, s, size);
+}
+
+castling_rights_t parse_castling_rights(const char *rights_string) {
+  castling_rights_t rights = CASTLE_NONE;
+  for (const char *p = rights_string; *p != '\0'; p++) {
+    switch (*p) {
+    case 'K':
+      rights |= CASTLE_WHITE_KING;
+      break;
+    case 'Q':
+      rights |= CASTLE_WHITE_QUEEN;
+      break;
+    case 'k':
+      rights |= CASTLE_BLACK_KING;
+      break;
+    case 'q':
+      rights |= CASTLE_BLACK_QUEEN;
+      break;
+    }
+  }
+  return rights;
+}
+
+void parse_pieces(char *pieces_string, piece_t out_pieces[64]) {
+  piece_t *current_piece = out_pieces;
+  for (char *rank = strtok(pieces_string, "/"); rank != NULL;
+       rank = strtok(NULL, "/")) {
+    for (size_t i = 0; rank[i] != '\0'; i++) {
+      // We only check [1, 8] rather than [0, 9] because fen strings cannot have
+      // zeroes or nines in them.
+      if ('1' <= rank[i] && rank[i] <= '8') {
+        for (size_t j = 0; j < rank[i] - '0'; j++) {
+          *current_piece++ = PIECE_NONE;
+        }
+      } else {
+        *current_piece++ = rank[i];
+      }
+    }
+  }
+}
