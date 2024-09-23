5a328b776b9d8190991b338f3ea0976531fcd7b7
Paul Zupan <paul.zupan.13@gmail.com>
2024-09-23 00:36:47 -0700

    Add raylib dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use raylib::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let (mut rl, thread) = raylib::init().size(800, 800).title("Chess").build();
+
+    rl.set_target_fps(60);
+
+    while !rl.window_should_close() {
+        let mut d = rl.begin_drawing(&thread);
+
+        d.clear_background(Color::WHITE);
+        d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
+    }
 }
